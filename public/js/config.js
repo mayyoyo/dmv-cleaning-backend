d8461f3853da48fc6d2a3b0c863b86037ca5a3e4
Genet <gtalema@gmail.com>
2026-06-22 22:50:07 -0400

    final backend fix

--- public/js/config.js
+++ public/js/config.js
@@ -1,2 +1,5 @@
 const API = "https://dmv-cleaning-backend.onrender.com/api";
+
 window.API = API;
+window.BASE_URL = "https://dmv-cleaning-backend.onrender.com";
+window.APP_NAME = "DMV Cleaning Services";
