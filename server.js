80cf597f4ad9d339f3ed5598bf236a7fa678a3ec
Genet <gtalema@gmail.com>
2026-06-23 01:40:02 -0400

    stable deployment fix

--- server.js
+++ server.js
@@ -251,3 +251,9 @@ const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log("🚀 Server running with WebSockets on port", PORT);
 });
+// 
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log("🚀 Server running with WebSockets on port", PORT);
+});
